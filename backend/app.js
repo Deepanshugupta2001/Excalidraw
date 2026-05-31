9485101bcea9a9d5a20f3b02a544bc12b8530959
Deepanshugupta2001 <deepanshugupta.dg.2001@gmail.com>
2026-05-31 23:46:09 +0530

    Backend Change

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            "https://excalidraw-eight-mu.vercel.app"
+            "https://excalidraw-opal-ten.vercel.app/"
         ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
